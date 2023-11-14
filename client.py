b1c10141f1e4a0d6cb2d8bd73a7c680fa13d843b
mulchus <mulch@list.ru>
2023-11-15 00:26:11 +0300

    On break connections raised socket.gaierror: [Errno 11001] getaddrinfo failed

--- client.py
+++ client.py
@@ -42,14 +42,44 @@ def fix_message(parser_args, message):
         logging.info(message)
     else:
         print(message)
+
+
+async def tcp_client(parser_args, host, port):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        data = await reader.readuntil(separator=b'\n')
+        message = f'{time.strftime("%d.%m.%Y %H:%M")}: {data.decode()}'
+        fix_message(parser_args, message)
         
+        # while not reader.at_eof():
+        #     data = await asyncio.wait_for(reader.read(100), 3.0)
+        #     print('raw data received: {}'.format(data))
+        
+    finally:
+        writer.close()
+
+
+async def tcp_reconnect(parser_args, host, port):
+    server = '{} {}'.format(host, port)
+    while True:
+        print('Connecting to server {} ...'.format(server))
+        try:
+            await tcp_client(parser_args, host, port)
+        except (ConnectionRefusedError, ConnectionAbortedError):
+            print('Connection to server {} failed!'.format(server))
+        except asyncio.TimeoutError:
+            print('Connection to server {} timed out!'.format(server))
+        else:
+            print('Connection to server {} is closed.'.format(server))
+        # await asyncio.sleep(2.0)
+
 
 async def main():
     parser_args = get_args()
     host = parser_args.host
     port = parser_args.port
     _logging = parser_args.logging
-    
+    print(_logging)
     if parser_args.logging:
         logging.basicConfig(
             filename='chat.txt',
@@ -57,27 +87,35 @@ async def main():
             level=logging.INFO
         )
     
-    reader, writer = await asyncio.open_connection(host, port)
+    coros = tcp_reconnect(parser_args, host, port)
+    await asyncio.gather(coros)
     
-    try:
-        # with open('chat.txt', '+a', encoding='utf-8') as file:
-        while True:
-            data = await reader.readuntil(separator=b'\n')
-            message = f'{time.strftime("%d.%m.%Y %H:%M")}: {data.decode()}'
-            fix_message(parser_args, message)
-            # file.writelines(message)
-    except asyncio.exceptions.CancelledError:
-        fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: Close the connection')
-    except IndexError:
-        fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: IndexError')
-    except SystemExit :
-        fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: SystemExit error')
-    except (ConnectionAbortedError, Exception) as error:
-        fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: {error}')
-    finally:
-        writer.close()
-        await writer.wait_closed()
+    # reader, writer = await asyncio.open_connection(host, port)
+    
+    # while True:
+    #     try:
+    #         # with open('chat.txt', '+a', encoding='utf-8') as file:
+    #         # while True:
+    #         data = await reader.readuntil(separator=b'\n')
+    #         message = f'{time.strftime("%d.%m.%Y %H:%M")}: {data.decode()}'
+    #         fix_message(parser_args, message)
+    #         # file.writelines(message)
+    #     except asyncio.exceptions.CancelledError:
+    #         fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: Close the connection')
+    #     except IndexError:
+    #         fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: IndexError')
+    #     except SystemExit :
+    #         fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: SystemExit error')
+    #     except (ConnectionAbortedError, Exception) as error:
+    #         fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M")}: {error}')
+    #         time.sleep(1)
+            # reader, writer = await asyncio.open_connection(host, port)
+        # finally:
+            # writer.close()
+            # await writer.wait_closed()
+            # reader, writer = await asyncio.open_connection(host, port)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+    # asyncio.create_task(main())
