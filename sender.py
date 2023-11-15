f5946b7854b4c49bc5c4400583cf4198388f328b
mulchus <mulch@list.ru>
2023-11-16 00:54:02 +0300

    Refactor code, split by reusable functions.

--- sender.py
+++ sender.py
@@ -1,10 +1,86 @@
 import asyncio
 import logging
+import json
 
 
 host = 'minechat.dvmn.org'
 port = 5050
 
+
+async def register(reader, writer):
+    data = await reader.readuntil(separator=b'\n')
+    # logging.info(f'Received: {data.decode()}')
+    
+    message = '\n'
+    writer.write(message.encode())
+    await writer.drain()
+    
+    data = await reader.readuntil(separator=b'\n')
+    # logging.info(f'Received: {data.decode()}')
+    
+    logging.info('Регистрация нового участника чата.')
+    await asyncio.sleep(.5)
+    while True:
+        user_nik = input('Введите ваш ник: ')
+        if len(user_nik):
+            user_nik += '\n'
+            break
+    
+    writer.write(user_nik.encode())
+    await writer.drain()
+    
+    new_user_encoding = await reader.readuntil(separator=b'\n')
+    
+    new_user = json.loads((new_user_encoding.decode()).split('\n')[0])
+    account_hash = new_user['account_hash']
+    with open('account_hash', 'w') as file:
+        file.write(account_hash)
+        file.close()
+    
+    data = await reader.readuntil(separator=b'\n')
+    # logging.info(f'Received: {data.decode()}')
+    
+    writer.close()
+    await writer.wait_closed()
+    logging.info('End registration!')
+    
+
+async def authorise(reader, writer):
+    data = await reader.readuntil(separator=b'\n')
+    # logging.info(f'Received: {data.decode()}')
+    
+    with open('account_hash', 'r') as file:
+        account_hash = file.read()
+    
+    writer.write((account_hash + '\n').encode())
+    await writer.drain()
+    
+    data = await reader.readuntil(separator=b'\n')
+    if 'null' in data.decode():
+        logging.info('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+        writer.close()
+        await writer.wait_closed()
+        return
+    else:
+        # logging.info(f'Received: {data.decode()}')
+        pass
+    
+    logging.info('End authorise!')
+    
+
+async def submit_message(reader, writer):
+    while True:
+        message = input('Введите сообщение: ')
+        if len(message):
+            message += '\n\n'
+            break
+    
+    writer.write(message.encode())
+    await writer.drain()
+    
+    data = await reader.readuntil(separator=b'\n')
+    # logging.info(f'Received: {data.decode()}')
+    
 
 async def main():
     logging.basicConfig(
@@ -13,40 +89,13 @@ async def main():
         encoding='utf-8',
         level=logging.INFO,
     )
-    
     reader, writer = await asyncio.open_connection(host, port)
-    data = await reader.readuntil(separator=b'\n')
-    logging.info(f'Received: {data.decode()}')
-    
-    with open('account_hash', 'r') as file:
-        account_hash = file.read()
+    await register(reader, writer)
+    reader, writer = await asyncio.open_connection(host, port)
+    await authorise(reader, writer)
+    while True:
+        await submit_message(reader, writer)
         
-    writer.write((account_hash + '\n').encode())
-    await writer.drain()
-
-    data = await reader.readuntil(separator=b'\n')
-    if 'null' in data.decode():
-        logging.info('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-        writer.close()
-        await writer.wait_closed()
-        return
-    else:
-        logging.info(f'Received: {data.decode()}')
-    
-    message = 'rECEIVED: hELLO %USERNAME%! ....\n\n'
-    writer.write(message.encode())
-    await writer.drain()
-    
-    data = await reader.readuntil(separator=b'\n')
-    logging.info(f'Received: {data.decode()}')
-    
-    message = 'вАШ КОД ОТПРАВЛЕН НА ПРОВЕРКУ. пРЕПОДАВАТЕЛЬ посмотрит и ответит в течение 1 рабочего дня..\n\n'
-    writer.write(message.encode())
-    await writer.drain()
-    
-    data = await reader.readuntil(separator=b'\n')
-    logging.info(f'Received: {data.decode()}')
-   
     writer.close()
     await writer.wait_closed()
 
