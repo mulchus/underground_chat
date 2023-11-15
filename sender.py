54e5f2a9cc1f4ea61801fdb9fd43c8a25de794cd
mulchus <mulch@list.ru>
2023-11-16 00:06:36 +0300

    Updated send message from user, who hash in file.

--- sender.py
+++ sender.py
@@ -15,14 +15,16 @@ async def main():
     )
     
     reader, writer = await asyncio.open_connection(host, port)
-    data = await reader.read(100)
+    data = await reader.readuntil(separator=b'\n')
     logging.info(f'Received: {data.decode()}')
     
-    message = '0a85606e-83ba-11ee-aae7-0242ac110002--\n'
-    writer.write(message.encode())
+    with open('account_hash', 'r') as file:
+        account_hash = file.read()
+        
+    writer.write((account_hash + '\n').encode())
     await writer.drain()
 
-    data = await reader.read(100)
+    data = await reader.readuntil(separator=b'\n')
     if 'null' in data.decode():
         logging.info('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
         writer.close()
@@ -35,14 +37,14 @@ async def main():
     writer.write(message.encode())
     await writer.drain()
     
-    data = await reader.read(100)
+    data = await reader.readuntil(separator=b'\n')
     logging.info(f'Received: {data.decode()}')
     
     message = 'вАШ КОД ОТПРАВЛЕН НА ПРОВЕРКУ. пРЕПОДАВАТЕЛЬ посмотрит и ответит в течение 1 рабочего дня..\n\n'
     writer.write(message.encode())
     await writer.drain()
     
-    data = await reader.read(100)
+    data = await reader.readuntil(separator=b'\n')
     logging.info(f'Received: {data.decode()}')
    
     writer.close()
