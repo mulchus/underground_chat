a76335cf5f0a9b73d3e39fa06af494b45c6db09b
mulchus <mulch@list.ru>
2023-11-15 16:13:31 +0300

    Added environs arguments. Added README.md.

--- client.py
+++ client.py
@@ -4,16 +4,12 @@ import logging
 import time
 import socket
 
+from environs import Env
+from pathlib import Path
 
-def get_args():
+
+def get_args(env):
     parser = argparse.ArgumentParser(description='Скрипт чтения подпольного чата')
-    parser.add_argument(
-        '--logging',
-        nargs='?',
-        type=bool,
-        default=False,
-        help='включить или выключить логирование'
-    )
     parser.add_argument(
         '--host',
         nargs='?',
@@ -29,31 +25,37 @@ def get_args():
         help='порт сервера чата'
     )
     parser.add_argument(
-        '--size',
+        '--history',
         nargs='?',
-        type=int,
-        default=100,
-        help='размер считываемого сообщения'
+        type=str,
+        help='относительный путь к файлу сообщений чата, включая имя файла'
     )
-    return parser.parse_args()
+
+    host = env('HOST') if env('HOST') else parser.parse_args().host
+    port = int(env('PORT')) if env('PORT') else parser.parse_args().port
+    history = env('HISTORY') if env('HISTORY') else parser.parse_args().history
+
+    return host, port, history
 
 
-def fix_message(parser_args, message):
-    if parser_args.logging:
+def fix_message(history, message):
+    if history:
         logging.info(message)
     else:
         print(message)
 
 
 async def main():
-    parser_args = get_args()
-    host = parser_args.host
-    port = parser_args.port
-    _logging = parser_args.logging
-    
-    if parser_args.logging:
+    env = Env()
+    env.read_env()
+    host, port, history = get_args(env)
+    if history:
+        Path.mkdir(Path.joinpath(Path.cwd(), history).parent, exist_ok=True)
+    print(f'Начинаем трансляцию из {host}:{port} в {Path.joinpath(Path.cwd(), history) if history else "терминал"}')
+
+    if history:
         logging.basicConfig(
-            filename='chat.txt',
+            filename=history,
             encoding='utf-8',
             level=logging.INFO
         )
@@ -65,20 +67,20 @@ async def main():
             try:
                 reader, writer = await asyncio.open_connection(host, port)
             except socket.gaierror as error:
-                fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M:%S")}: Ошибка домена (IP адреса) {error}')
+                fix_message(history, f'{time.strftime("%d.%m.%Y %H:%M:%S")}: Ошибка домена (IP адреса) {error}')
                 await asyncio.sleep(3)
         else:
             try:
                 while True:
                     data = await reader.readuntil(separator=b'\n')
                     message = f'{time.strftime("%d.%m.%Y %H:%M:%S")}: {data.decode()}'
-                    fix_message(parser_args, message)
+                    fix_message(history, message)
             except ConnectionAbortedError as error:
-                fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M:%S")}: ConnectionAbortedError {error}')
+                fix_message(history, f'{time.strftime("%d.%m.%Y %H:%M:%S")}: ConnectionAbortedError {error}')
                 reader = None
                 writer.close()
             except asyncio.exceptions.CancelledError as error:
-                fix_message(parser_args, f'{time.strftime("%d.%m.%Y %H:%M:%S")}: CancelledError {error}')
+                fix_message(history, f'{time.strftime("%d.%m.%Y %H:%M:%S")}: CancelledError {error}')
 
 
 if __name__ == "__main__":
